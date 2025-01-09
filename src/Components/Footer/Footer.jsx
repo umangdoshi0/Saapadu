ab00a2ffc69336d1ecf032ab59d1b49f8475cd7f
Priya Gupta <priyagupta052k@gmail.com>
2025-01-09 16:28:22 +0530

    Update Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,55 +1,40 @@
 import React from "react";
-// import mainlogo from '../../assets/logo.png';
-import './Footer.css';
+import "./Footer.css";
+
 function Footer() {
-    return (
-        <section>
-            <hr />
-            <footer className="footer">
-                <div>
-                    {/* <div>
-                        <img src={mainlogo} alt="LOGO" className='navlogo' />
-                    </div> */}
-                    <div>
-                        <h3 style={{ fontWeight: "bolder" }}>VIT Sapaadu</h3>
-                    </div>
-                    <div>
-                        <p>2024 Limited</p>
-                    </div>
-                </div>
-                <div className="footer-section">
-                    <h2>Company</h2>
-                </div>
-                <div className="footer-section">
-                    <p><a href="#About Us">About Us</a></p>
-                </div>
-                <div className="footer-section">
-                    <p><a href="#Team">Team</a></p>
-                </div>
-                <div className="footer-section">
-                    <h2>Contact Us</h2>
-                </div>
-                <div className="footer-section">
-                    <p><a href='#Help & Support'>Help & Support</a></p>
-                </div>
-                <div className="footer-section">
-                    <h2>Legal</h2>
-                </div>
-                <div className="footer-section">
-                    <p><a href="#Terms & Conditions">Terms & Conditions</a></p>
-                </div>
-                <div className="footer-section">
-                    <p><a href="#Privacy Policy">Privacy Policy</a></p>
-                </div>
-                <div className="footer-section">
-                    <h2>Available in:</h2>
-                </div>
-                <div className="footer-section">
-                    <p>VIT Chennai</p>
-                </div>
-            </footer>
-        </section>
-    );
+  return (
+    <>
+        {/* <hr /> */}
+      <footer className="footer">
+    <div className="section">
+      <div className="heading">
+       <h3>VIT Sapaadu</h3>
+      </div>
+        <div className="section">
+          <div className="footer-section">
+            <h2>Company</h2>
+            <p>
+              <a href="#About Us">About Us</a>
+            </p>
+            <p>
+              <a href="#Team">Team</a>
+            </p>
+          </div>
+          <div className="footer-section">
+            <h2>Contact Us</h2>
+            <p>
+              <a href="#Help & Support">Help & Support</a>
+            </p>
+          </div>
+          <div className="footer-section">
+          <h2>Available in:</h2>
+            <p>VIT Chennai</p>
+          </div>
+        </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
