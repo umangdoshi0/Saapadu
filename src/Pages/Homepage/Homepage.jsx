a0b0d31ca186e19fd9e5fbd4af42287e36d52638
Priya Gupta <priyagupta052k@gmail.com>
2025-01-09 10:22:56 +0530

    Update Footer

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
-
+import Footer from "../../Components/Footer/Footer";
 
 
 function Homepage() {
     return (
         <>
             <Navbar />
-            Hello
+            <Footer />
         </>
     );
 }
