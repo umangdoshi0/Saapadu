8cd9c32c60f9842f41282451218a1223c8b09de4
Umang Doshi <umangdoshi151@gmail.com>
2025-01-09 16:27:33 +0530

    updated navbar on other pages

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 // Cart.jsx
 import React from 'react';
 import './Cart.css';
+import Navbar from "../../Components/Navbar/Navbar";
 // import { FaUser } from "react-icons/fa";
 // import { useNavigate } from 'react-router-dom';
 
@@ -15,36 +16,39 @@ const Cart = ({ cartItems, totalBill, removeFromCart, updateItemQuantity }) => {
     };
 
     return (
-        <div className="checkout-wrapper">
-            <nav>
-                <div className="navbar-logo">
-                    <h1>VIT Sapaadu</h1>
-                </div>
-                {/* <div className="navbar-user">
+        <div>
+            <Navbar />
+            <div className="checkout-wrapper">
+                <nav>
+                    <div className="navbar-logo">
+                        <h1>VIT Sapaadu</h1>
+                    </div>
+                    {/* <div className="navbar-user">
                     <button onClick={handleAccountClick} type="submit" className="user-button" name="user-button"><FaUser className="icon" />USER</button>
                 </div> */}
-            </nav>
-            <div className="checkout-container">
-                <h2 className="checkout-title">Checkout</h2>
-                {cartItems.length === 0 ? (
-                    <p>Your cart is empty.</p>
-                ) : (
-                    cartItems.map(item => (
-                        <div key={item._id} className="cart-item">
-                            <img src={item.image} alt={item.name} />
-                            <h3>{item.name}</h3>
-                            <p>Price: ₹{item.price}</p>
-                            <p>Quantity: {item.quantity}</p>
-                            <button onClick={() => updateItemQuantity(item._id, item.quantity + 1)}>+</button>
-                            <button onClick={() => updateItemQuantity(item._id, item.quantity - 1)}>-</button>
-                            <button onClick={() => removeFromCart(item._id)}>Remove</button>
-                        </div>
-                    ))
-                )}
-                <h3>Total: ₹{totalBill}</h3>
-                <button onClick={handleCheckout} className="checkout-button">
-                    Proceed to Payment
-                </button>
+                </nav>
+                <div className="checkout-container">
+                    <h2 className="checkout-title">Checkout</h2>
+                    {cartItems.length === 0 ? (
+                        <p>Your cart is empty.</p>
+                    ) : (
+                        cartItems.map(item => (
+                            <div key={item._id} className="cart-item">
+                                <img src={item.image} alt={item.name} />
+                                <h3>{item.name}</h3>
+                                <p>Price: ₹{item.price}</p>
+                                <p>Quantity: {item.quantity}</p>
+                                <button onClick={() => updateItemQuantity(item._id, item.quantity + 1)}>+</button>
+                                <button onClick={() => updateItemQuantity(item._id, item.quantity - 1)}>-</button>
+                                <button onClick={() => removeFromCart(item._id)}>Remove</button>
+                            </div>
+                        ))
+                    )}
+                    <h3>Total: ₹{totalBill}</h3>
+                    <button onClick={handleCheckout} className="checkout-button">
+                        Proceed to Payment
+                    </button>
+                </div>
             </div>
         </div>
     );
