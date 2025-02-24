4db421794c1caccae24859780049bf49e2e7b373
Priya Gupta <priyagupta052k@gmail.com>
2025-02-24 23:51:50 +0530

    Update Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-// import Login from './Pages/Login/Login';
-// import SignUp from "./Pages/Login/SignUp";
+import Login from './Pages/Login/Login';
+import SignUp from "./Pages/Login/SignUp";
 import Home from "./Pages/Homepage/Homepage";
 import Menu from "../src/Pages/Menu/Menu";
 import Cart from "../src/Pages/Cart/Cart";
@@ -78,9 +78,9 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/signup" element={<SignUp />} /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           {/* <Route path="/home" element={<Home />} /> */}
           {/* <Route path="*" element={<Navigate to="/login" />} /> */}
           <Route
