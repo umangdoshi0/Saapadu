43848a448ee46e1ea97e0f8e8478b23b47018aaa
Priya Gupta <priyagupta052k@gmail.com>
2025-03-05 21:14:43 +0530

    update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const startServer = async () => {
         // Start Server
         const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => {
-            console.log(Server is running on port ${PORT});
+            console.log(`Server is running on port ${PORT}`);
         });
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
