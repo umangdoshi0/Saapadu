b5e8476a3ef6300dc6bacd3cbf77a872c8094330
Umang Doshi <umangdoshi151@gmail.com>
2025-04-13 00:12:13 +0530

    added aws transcribe

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -101,30 +101,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-
-
-// import React from "react";
-// import "./Navbar.css";
-// import Logo from "../Navbar/Logo/Logo"
-// import { Link } from "react-router-dom";
-// import { IoCart } from "react-icons/io5";
-// import { FaUser } from "react-icons/fa";
-
-// function Navbar() {
-//     return (
-//         <div className="navbar">
-//             <div className="navleft">
-//                 {/* <Image />
-//                 <Text /> */}
-//                 <Logo />
-                
-//             </div>
-//             <div className="navright">
-//                 <Link to="/cart"><IoCart className="cart-icon"/></Link>
-//                 <Link to ="/account"><FaUser className="acct-icon"/></Link>
-//             </div>
-//         </div>
-//     );
-// }
-// export default Navbar;
