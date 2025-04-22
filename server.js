095746aa47a18729b8d5f1d97d00695f8498d465
Umang Doshi <umangdoshi151@gmail.com>
2025-04-22 14:30:03 +0530

    changes

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ const startServer = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
         const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
+        app.listen(PORT , '0.0.0.0', () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (err) {
